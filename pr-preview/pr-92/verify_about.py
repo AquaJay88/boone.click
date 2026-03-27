3ab37bec73564c0ee6d42a62ccaee8398383f1b5
AquaJay88 <AquaJay88@users.noreply.github.com>
2026-03-27 19:50:06 +0000

    Remove preview for PR 92 🛬

--- pr-preview/pr-92/verify_about.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page(viewport={'width': 800, 'height': 800})
-    page.goto('http://localhost:8000/store/about.html')
-    # wait a bit for fonts/layout
-    page.wait_for_timeout(1000)
-    page.screenshot(path='about_mobile.png')
-    browser.close()
