8abdcf58841a08fd6fe04d695aed20f08de3eb7d
AquaJay88 <AquaJay88@users.noreply.github.com>
2026-03-27 12:00:25 +0000

    Remove preview for PR 76 🛬

--- pr-preview/pr-76/store/supabase.js
+++
@@ -1,7 +0,0 @@
-// supabase.js
-// Initialize Supabase Client
-
-const SUPABASE_URL = 'https://alfszmccbxndsrronyfe.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_0pylFOnLWSPB7M2tudQU_w_eYPSc9FQ';
-
-window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
