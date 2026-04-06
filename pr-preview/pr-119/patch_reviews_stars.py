8120ff90957cf3baf5d67db203b77aeffec803f4
AquaJay88 <AquaJay88@users.noreply.github.com>
2026-04-06 21:55:15 +0000

    Remove preview for PR 119 🛬

--- pr-preview/pr-119/patch_reviews_stars.py
+++
@@ -1,7 +0,0 @@
-with open('store/index.html', 'r') as f:
-    content = f.read()
-
-content = content.replace('      .review-stars {\n        color: var(--primary-color);\n        margin-bottom: 1rem;\n      }', '      .review-stars {\n        color: var(--primary-color);\n        margin-bottom: 0.5rem;\n      }')
-
-with open('store/index.html', 'w') as f:
-    f.write(content)
