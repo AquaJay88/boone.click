c7461126a68819bc1539346709331474cddc88bd
AquaJay88 <AquaJay88@users.noreply.github.com>
2026-05-04 11:55:21 +0000

    Remove preview for PR 124 🛬

--- pr-preview/pr-124/fix_init_reviews.py
+++
@@ -1,9 +0,0 @@
-with open('store/index.html', 'r') as f:
-    content = f.read()
-
-content = content.replace('            updateDisplay();\n      initReviews();', '            updateDisplay();')
-content = content.replace('        updateDisplay();\n      initReviews();', '        updateDisplay();')
-content = content.replace('          updateDisplay();\n      initReviews();', '          updateDisplay();')
-
-with open('store/index.html', 'w') as f:
-    f.write(content)
